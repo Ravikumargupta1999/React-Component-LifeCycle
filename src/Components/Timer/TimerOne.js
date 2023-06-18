bc79a4c621aec4ca35267310cd3bf8e09dd9fd66
unknown <ravikumargupta391999@gmail.com>
2023-06-18 20:25:30 +0530

    Order of excution

--- src/Components/Timer/TimerOne.js
+++ src/Components/Timer/TimerOne.js
@@ -3,34 +3,52 @@ import React from "react";
 export default class TimerOne extends React.Component {
   constructor() {
     super();
-    console.log("Timer Constructor");
+    console.log("TimerOne Constructor");
+    this.state = {
+      count : 0
+    }
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log("Timer getDerivedStateFromProps");
+    console.log("TimerOne getDerivedStateFromProps");
     return null;
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log("Timer shouldComponentUpdate");
     return true;
+  }
+
+  handleIncrease = () =>{
+    this.setState((prevState) =>{
+      return{
+        count : prevState.count + 1
+      }
+    })
   }
 
   render(){
     console.log('TimerOne Render');
-    return <h1>Timer</h1>
+    return(
+      <>
+         <h1>C</h1>
+         <h2>{this.state.count}</h2>
+         <button onClick={this.handleIncrease}>Increase</button>
+      </>
+    )
   }
   componentDidMount() {
-    console.log("Timer ComponentDidMount");
+    console.log("TimerOne componentDidMount");
+    console.log('______________________________')
+    
   }
 
   getSnapshotBeforeUpdate(prevProp, prevState) {
-    console.log("Timer getSnapshotBeforeUpdate");
+    console.log("TimerOne getSnapshotBeforeUpdate");
     return null;
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("Timer componentDidUpdate");
+    console.log("TimerOne componentDidUpdate");
   }
 
  
