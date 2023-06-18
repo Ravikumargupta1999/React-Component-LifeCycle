1fcd746760db62593d6eee6cb4734d80bd7a2a82
unknown <ravikumargupta391999@gmail.com>
2023-06-18 23:31:02 +0530

    Controlling The Timer

--- src/Components/Timer/TimerOne.js
+++ src/Components/Timer/TimerOne.js
@@ -25,24 +25,16 @@ export default class TimerOne extends React.Component {
     console.log('TimerOne Render');
     return (
       <>
-        <h1>Time Spent : {new Date(this.state.time * 1000).toISOString().slice(11,19)}</h1>
-        
-
+        <h1>Time Spent :
+          {new Date(this.state.time * 1000).toISOString().slice(11, 19)}
+        </h1>
       </>
     )
   }
-  componentDidMount() {
-    console.log("TimerOne componentDidMount");
-    console.log('______________________________')
 
-    this.timer = setInterval(() => {
-      this.setState((prevState) => ({
-        time: prevState.time + 1
-      }))}, 1000);
 
-  }
 
-  getSnapshotBeforeUpdate(prevProp, prevState) {
+  getSnapshotBeforeUpdate(prevProps, prevState) {
     console.log("TimerOne getSnapshotBeforeUpdate");
     return null;
   }
@@ -50,17 +42,27 @@ export default class TimerOne extends React.Component {
   componentDidUpdate(prevProps, prevState, snapshot) {
     console.log("TimerOne componentDidUpdate");
     console.log('________________________________');
-    console.log(this.state.time,typeof(this.state.time))
+    // console.log('Previous Props',prevProps);
+    // console.log('Previous State',prevState);
+    // console.log('Snap Shot',snapshot);
 
-    // if(this.state.time == 10){
-    //   clearInterval(this.time)
-    // }
+
+    if (prevProps.timerOn !== this.props.timerOn) {
+      if (this.props.timerOn) {
+        this.timer = setInterval(() => {
+          this.setState((prevState) => ({ time: prevState.time + 1 }))
+        }, 1000);
+
+      } else {
+        clearInterval(this.timer)
+      }
+    }
   }
 
 
-  componentWillUnmount(){
-    console.log('TimerOne componentWillUnmount');
-    clearInterval(this.timer);
-  }
+    componentWillUnmount() {
+      console.log('TimerOne componentWillUnmount');
+      clearInterval(this.timer);
+    }
 
-}
+  }
